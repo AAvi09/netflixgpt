673c1aff7f728bce7894172f0075a4fbf17cc558
Avinash sharma <sharmaavi2196@gmail.com>
2024-12-28 02:44:39 +0530

    logo and background image of netflix fetched

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-black">
+    <div className="absolute ">
       <img
-        src="https://xxboxnews.blob.core.windows.net/prod/sites/2/2023/05/Background-size1920x1080-4e1694a6-75aa-4c36-9d4d-7fb6a3102005-bc5318781aad7f5c8520.png"
-        alt="logo"
+        className="w-36"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Netflix Logo"
       />
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,18 @@ import Header from "./Header";
 const Login = () => {
   return (
     <div>
+      <div className=" w-full h-screen bg-cover bg-center bg-no-repeat overflow-hidden -z-1">
+        <img
+          className="  top-0 left-0 w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg"
+          srcSet="
+        https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg 1000w,
+        https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_medium.jpg 1500w,
+        https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_large.jpg 1800w
+      "
+          alt="Netflix Background"
+        />
+      </div>
       <Header />
     </div>
   );
