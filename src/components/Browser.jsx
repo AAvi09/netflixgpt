a7e1fc616018e0315688d236ccd58a5266d639e5
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-03 02:25:17 +0530

    tmdb api successfully data fetched

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useSelector } from "react-redux";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constants";
 
 const Browser = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
   const handleSignOut = () => {
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjUwYTRkODRmNjM1ZTMxNmUxYTg5MjI0ODE4MGQ3NCIsIm5iZiI6MTczNTg0OTg5Ni4xMzMsInN1YiI6IjY3NzZmN2E4MzEzMzgzZDJhZDcyM2FhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ip_mwokhqgsV0qiJpuIrG9H8vO-rLACb0R760_5kTNc",
+  },
+};
