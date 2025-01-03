5b2f8459d64a8d4f98a564717396e79b8c06b0d5
Avinash sharma <sharmaavi2196@gmail.com>
2025-01-04 02:11:29 +0530

    started building secondary container for the now playing row created movielist and moviecard skeleton

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movies);
+  return (
+    <div>
+      <MovieList title={"now playing"} movies={movies.nowPlayingMovies} />
+    </div>
+  );
 };
 
 export default SecondaryContainer;
